02787f534ad97cd4eb4d2d2a82cdcfbf19fc3813
Antony-AK <antonyak147@gmail.com>
2026-02-04 14:18:10 +0530

    Update tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
+      }
+    }
+  },
+  plugins: []
+}
