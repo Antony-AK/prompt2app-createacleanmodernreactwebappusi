1623cdcb73b27e1a74fc40ecb2d7d639bc8107c7
Antony-AK <antonyak147@gmail.com>
2026-02-04 14:18:28 +0530

    Update src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className='bg-white/5 py-8 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-7xl mx-auto text-center text-gray-400'>
+        © 2024 ModernApp. All rights reserved.
+      </div>
+    </footer>
+  )
+}
